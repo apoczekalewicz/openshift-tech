0e460f056ae9ddb6c6d23e4deff2556658a59260
Artur <artur@poczekalewicz.net>
2023-11-17 21:25:05 +0100

    commit

--- operators/DR/scripts/ssl/ssl.sh
+++ operators/DR/scripts/ssl/ssl.sh
@@ -1,15 +1,15 @@
-c1
+cluster1
 oc get cm default-ingress-cert -n openshift-config-managed -o jsonpath="{['data']['ca-bundle\.crt']}" > primary.crt
-c2
+cluster2
 oc get cm default-ingress-cert -n openshift-config-managed -o jsonpath="{['data']['ca-bundle\.crt']}" > secondary.crt
 
 cat primary.crt secondary.crt > all.crt
 oc create --dry-run=client -n openshift-config cm user-ca-bundle -o yaml --from-file=ca-bundle.crt=./all.crt > cm-clusters-crt.yaml
 
-c1
+cluster1
 oc create -f cm-clusters-crt.yaml
 oc patch proxy cluster --type=merge  --patch='{"spec":{"trustedCA":{"name":"user-ca-bundle"}}}'
-c2
+cluster2
 oc create -f cm-clusters-crt.yaml
 oc patch proxy cluster --type=merge  --patch='{"spec":{"trustedCA":{"name":"user-ca-bundle"}}}'
 
